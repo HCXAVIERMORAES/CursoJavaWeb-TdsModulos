b7b81c017e1ea78fc66b84d0f2b7445d6a816dae
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2021-07-09 21:26:25 -0300

    idem

--- TdsModulosCursoJavaWeb/modulo16/arquivo/Aquivos.java
+++ TdsModulosCursoJavaWeb/modulo16/arquivo/Aquivos.java
@@ -37,16 +37,16 @@ public class Aquivos {
 		File arquivo = new File("C:\\Users\\hphoe\\git\\repository5\\.git\\TdsModulosCursoJavaWeb\\modulo16\\arquivo\\arquivo.txt");
 
 		//condição de existencia
-		if(!arquivo.exists()) {//se não existir
-			arquivo.createNewFile();//cria o arqquivo
-		}
+			if(!arquivo.exists()) {//se não existir
+				arquivo.createNewFile();//cria o arqquivo
+			}
 		
 		//escrever no arquivo
 		FileWriter escrever_arquivo = new FileWriter(arquivo);
 		escrever_arquivo.write("Meu texto no arqquivo 1ª linha");
 		escrever_arquivo.write("\n"+"Minha 2ª linha");
 		
-		//com for
+		//escrevendo
 		for (int i = 1; i <= 10; i++) {
 			escrever_arquivo.write("\n"+"Texto da minha linha "+i);
 		}
