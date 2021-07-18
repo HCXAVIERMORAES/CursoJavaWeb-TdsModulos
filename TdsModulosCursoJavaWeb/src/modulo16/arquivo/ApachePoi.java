71543dec7bc3f9a3b66dc8d1168a8f331d1a7753
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2021-07-17 22:34:09 -0300

    criando planilha excel xml

---
+++ TdsModulosCursoJavaWeb/src/modulo16/arquivo/ApachePoi.java
@@ -0,0 +1,73 @@
+package modulo16.arquivo;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachePoi {
+
+	public static void main(String[] args) throws Exception {
+		/*Para trabalhar com arquivos em excel usa-se o apache POI e se baixa o arquivo jar poi-*.jar
+		 * para usa-lo
+		 * */
+		
+		//criando arquivo
+		File file = new File("C:\\Users\\hphoe\\git\\repository5\\.git\\TdsModulosCursoJavaWeb"
+				+ "\\src\\modulo16\\arquivo");// endereço de onde vaificar o arquivo a ser criado
+		
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		
+		//dados para escrer na planilha
+		Pessoa pessoa1 = new Pessoa();
+		pessoa1.setEmail("pessoa1@gmail.com");
+		pessoa1.setIdade("38");
+		pessoa1.setNome("Jose");
+		
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setEmail("pessoa2@gmail.com");
+		pessoa2.setIdade("18");
+		pessoa2.setNome("Ana");
+		
+		Pessoa pessoa3 = new Pessoa();
+		pessoa3.setEmail("pessoa3@gmail.com");
+		pessoa3.setIdade("30");
+		pessoa3.setNome("Marcos");
+		
+		//pode vir do banco de dados ou qualquer outra fonte de dados
+		List<Pessoa> pessoas = new  ArrayList<Pessoa>();		
+		//adicionando a lista
+		pessoas.add(pessoa1);
+		pessoas.add(pessoa2);
+		pessoas.add(pessoa3);
+		
+		//usado para escrever na planilha
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook();
+		
+		//criar a planilha
+		HSSFSheet linhasPessoa = hssfWorkbook.createSheet("Planilha Pessoas JDev Treinamentos");
+		
+		//contra-se o numero de linhas for do foreach
+		int numeroLinhas = 0;
+		
+		//percorrendo a planilha
+		for (Pessoa p : pessoas) {
+			Row linha = linhasPessoa.createRow(numeroLinhas++); //criando a linha na planilha
+			
+			//controlando celula
+			int celula = 0;
+			
+			Cell celNome = linha.createCell(celula ++);// varaiavel de controle
+;			
+		}
+
+	}
+
+}
