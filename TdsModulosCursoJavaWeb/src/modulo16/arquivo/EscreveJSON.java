e4f04db452de26956338a7893c064e3a9c6480c3
HCXAVIERMORAES <hcxaviermoraes@outlook.com>
2022-10-14 20:19:55 -0300

    comite mes outubro

--- TdsModulosCursoJavaWeb/src/modulo16/arquivo/EscreveJSON.java
+++ TdsModulosCursoJavaWeb/src/modulo16/arquivo/EscreveJSON.java
@@ -1,11 +1,8 @@
 package modulo16.arquivo;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.List;
 
